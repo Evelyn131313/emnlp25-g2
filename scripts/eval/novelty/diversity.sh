3bc8044b1cf60cf001821ba53b86479c3d09c99b
Evelyn131313 <liuwx2023@shanghaitech.edu.cn>
2026-05-26 16:20:56 -0500

    diversity scripts

--- scripts/eval/novelty/diversity.sh
+++ scripts/eval/novelty/diversity.sh
@@ -1,2 +1,2 @@
-dir_file=results/novelty/g2_theta0.3_temp1.0
+dir_file=results/novelty/g2_theta0.3_temp1/generations.jsonl
 CUDA_VISIBLE_DEVICES=0 python eval/calculate_div.py --file ${dir_file} --task curated
--- scripts/eval/xlsum/diversity.sh
+++ scripts/eval/xlsum/diversity.sh
@@ -1,2 +1,2 @@
-dir_file=results/xlsum/theta0.3_temp1.0
+dir_file=results/xlsum/theta0.3_temp1.0/all_answer.jsonl
 CUDA_VISIBLE_DEVICES=0 python eval/calculate_div.py --file ${dir_file} --task xlsum
